1e233efa6ee1148519c19667aac12e0388586e03
Spenser Black <me@spenser.black>
2026-07-24 22:06:57 -0400

    Format

--- src/scripts/nicknames.ts
+++ src/scripts/nicknames.ts
@@ -171,8 +171,6 @@ export const suffixes: string[] = [
   "of the Western Pass",
 ];
 
-export const pickPrefix: (seed?: string) => string =
-  createPick(prefixes);
+export const pickPrefix: (seed?: string) => string = createPick(prefixes);
 export const pickName: (seed?: string) => string = createPick(names);
-export const pickSuffix: (seed?: string) => string =
-  createPick(suffixes);
+export const pickSuffix: (seed?: string) => string = createPick(suffixes);
