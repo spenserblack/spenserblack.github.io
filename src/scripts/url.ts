6dc313bfe4031a87c4783a8553481210a7b02301
Spenser Black <me@spenser.black>
2026-07-27 19:58:49 -0400

    Add share link for nickname from ID (#30)

---
+++ src/scripts/url.ts
@@ -0,0 +1,6 @@
+/**
+ * Gets the location, but only in the client.
+ * @returns The window's URL.
+ */
+export const getLocationUrl: () => URL | null = () =>
+  import.meta.env.SSR ? null : new URL(window.location.href);
